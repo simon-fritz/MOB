92161d96a0f48dca717ceadfdbdb0bfd268b2f5a
Simon Fritz <simonfr09@gmail.com>
2025-08-07 19:49:25 +0200

    fix: improved data export in admin for guess model

--- backend/rooms/admin.py
+++ backend/rooms/admin.py
@@ -1,17 +1,27 @@
 from django.contrib import admin
 from .models import Room, RoomMembership, Match, Guess, ChatMessageLog
 from import_export.admin import ImportExportModelAdmin
+from import_export import resources, fields
 
 admin.site.register(Room)
 admin.site.register(RoomMembership)
 admin.site.register(Match)
 
+class GuessResource(resources.ModelResource):
+    user = fields.Field(
+        column_name='user',
+        attribute='user__username'
+    )
+    class Meta:
+        model = Guess
+        fields = ('id', 'user', 'round', 'guessed_ai', 'is_correct', 'created_at')
+
 class GuessAdmin(ImportExportModelAdmin):
-  ...
+    resource_class = GuessResource
 
 admin.site.register(Guess, GuessAdmin)
 
 class ChatMessageLogAdmin(ImportExportModelAdmin):
-  ...
+    pass
 
 admin.site.register(ChatMessageLog, ChatMessageLogAdmin)
