e0bc439b5704f175a13caadbec9d0610ea2b0b6f
Simon Fritz <simonfr09@gmail.com>
2025-08-09 16:43:18 +0200

    update: improved admin interface for CustomUser

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -1,4 +1,9 @@
 from django.contrib import admin
 from .models import CustomUser
 
-admin.site.register(CustomUser)
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ['id', 'username', 'email', 'role', 'first_name', 'last_name', 'is_active', 'date_joined']
+    list_filter = ['role', 'is_active', 'is_staff', 'date_joined']
+    search_fields = ['username', 'email', 'first_name', 'last_name']
+    readonly_fields = ['id', 'date_joined', 'last_login']
